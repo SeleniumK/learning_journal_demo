1a322426121ef492b3002a3d3329e43ef65cf47b
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-12-19 11:02:28 -0800

    adusted Configurator to include jinja2, fixed docstring to comply with pep8

--- learning_journal_basic/__init__.py
+++ learning_journal_basic/__init__.py
@@ -2,10 +2,9 @@ from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
+    """Return a Pyramid WSGI application."""
     config = Configurator(settings=settings)
-    config.include('pyramid_chameleon')
+    config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.scan()
